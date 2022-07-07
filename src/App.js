92d60a57306f0062f447e744f9252f7d7397c731
Jon Eastin <picardncce@gmail.com>
2022-07-07 08:39:30 -0700

    finished structuring the folder and adding the images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Navbar from "./components/Navbar"
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Card
+				img='katie_zaferes.png'
+				rating='5.0'
+				reviewCount={6}
+				location='USA'
+				title='Life lessons with Katie Zaferes'
+				price={136}
+			/>
+		</div>
+	)
 }
 
-export default App;
+export default App
